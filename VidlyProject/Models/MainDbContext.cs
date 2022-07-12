5a91d0bfaa62ceea77d503acb04bd40d99e5f768
shielamaebig <shielamaemalaque@gmail.com>
2022-07-12 09:14:30 +0800

    Add project files.

---
+++ VidlyProject/Models/MainDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using VidlyProject.Models;
+
+namespace VidlyProject.Models
+{
+    public class MainDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<MembershipType> MembershipTypes { get; set; }
+    }
+}
---
+++ VidlyProject/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VidlyProject.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Movies { get; set; }
+        public string Genre { get; set; }
+
+    }
+}

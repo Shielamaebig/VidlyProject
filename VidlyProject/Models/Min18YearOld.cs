6b38c4de391bb4542d8793ca4d3efd6efaebe11d
shielamaebig <shielamaemalaque@gmail.com>
2022-07-13 13:30:59 +0800

    Comments How the ModelMin18YrsOld run.

--- VidlyProject/Models/Min18YearOld.cs
+++ VidlyProject/Models/Min18YearOld.cs
@@ -11,7 +11,11 @@ namespace VidlyProject.Models
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
             var customer = (Customer)validationContext.ObjectInstance;
-
+            //from model Min18yrOld
+            //more readable than == 0 and == 1
+            //Creating this Model Min18 for Validating the Age of the customer thru the year today vs input birthday
+            //When the customer is not equal to 18 then he/she pic monthly quarterly or annual the result will Customer should be at least 18 year old
+            //When the customer is not 18 then the membership type is pay As you go the result is success
             if (customer.MembershipTypeId == MembershipType.Unknown ||
                 customer.MembershipTypeId == MembershipType.PayAsYouGo)
                 return ValidationResult.Success;
